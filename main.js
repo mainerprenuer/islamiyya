6edbc97e253d2cf96f6005aee7ed4f4b3a5871f3
Mousty Mainer <moustymainer@gmail.com>
2023-01-22 00:53:57 +0100

    working on course component

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // change navbar styles on scroll
 
-windows.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 100)
 }) 
